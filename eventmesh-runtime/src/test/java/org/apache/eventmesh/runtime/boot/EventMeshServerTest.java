6a4448ddbb5fead6aba60783c314193165f99ff7
eight-nines <1748526511@qq.com>
2022-12-11 19:33:18 +0800

    # EventMeshTCPConfiguration config

--- eventmesh-runtime/src/test/java/org/apache/eventmesh/runtime/boot/EventMeshServerTest.java
+++ eventmesh-runtime/src/test/java/org/apache/eventmesh/runtime/boot/EventMeshServerTest.java
@@ -7,6 +7,7 @@ import org.apache.eventmesh.common.config.CommonConfiguration;
 import org.apache.eventmesh.common.config.ConfigService;
 import org.apache.eventmesh.runtime.configuration.EventMeshGrpcConfiguration;
 import org.apache.eventmesh.runtime.configuration.EventMeshHTTPConfiguration;
+import org.apache.eventmesh.runtime.configuration.EventMeshTCPConfiguration;
 import org.junit.Assert;
 
 import java.util.ArrayList;
@@ -136,5 +137,64 @@ public class EventMeshServerTest extends TestCase {
         Assert.assertEquals(config.eventMeshServerUseTls, Boolean.TRUE);
         Assert.assertEquals(config.eventMeshBatchMsgRequestNumPerSecond, 21816);
         Assert.assertEquals(config.eventMeshMsgReqNumPerSecond, 19816);
+    }
+
+    public void testGetConfigForEventMeshTCPConfiguration() throws Exception {
+
+        ConfigService configService = ConfigService.getInstance();
+        configService.setRootConfig("classPath://newConfiguration-runtime.properties");
+
+        EventMeshTCPConfiguration config = configService.getConfig(EventMeshTCPConfiguration.class);
+
+        Assert.assertEquals(config.eventMeshEnv, "env-succeed!!!");
+        Assert.assertEquals(config.eventMeshIDC, "idc-succeed!!!");
+        Assert.assertEquals(config.eventMeshCluster, "cluster-succeed!!!");
+        Assert.assertEquals(config.eventMeshName, "name-succeed!!!");
+        Assert.assertEquals(config.sysID, "sysid-succeed!!!");
+        Assert.assertEquals(config.eventMeshConnectorPluginType, "connector-succeed!!!");
+        Assert.assertEquals(config.eventMeshSecurityPluginType, "security-succeed!!!");
+        Assert.assertEquals(config.eventMeshRegistryPluginType, "registry-succeed!!!");
+        Assert.assertEquals(config.eventMeshTracePluginType, "trace-succeed!!!");
+        Assert.assertEquals(config.eventMeshServerIp, "hostIp-succeed!!!");
+
+        List<String> list = new ArrayList<>();
+        list.add("metrics-succeed1!!!");
+        list.add("metrics-succeed2!!!");
+        list.add("metrics-succeed3!!!");
+        Assert.assertEquals(config.eventMeshMetricsPluginType, list);
+
+        Assert.assertEquals(config.eventMeshServerSecurityEnable, Boolean.TRUE);
+        Assert.assertEquals(config.eventMeshServerRegistryEnable, Boolean.TRUE);
+        Assert.assertEquals(config.eventMeshServerTraceEnable, Boolean.TRUE);
+
+        Assert.assertEquals(config.eventMeshWebhookOrigin, "eventmesh.idc-succeed!!!");
+
+
+        Assert.assertEquals(config.eventMeshTcpServerPort, 816);
+        Assert.assertEquals(config.eventMeshTcpIdleAllSeconds, 1816);
+        Assert.assertEquals(config.eventMeshTcpIdleWriteSeconds, 2816);
+        Assert.assertEquals(config.eventMeshTcpIdleReadSeconds, 3816);
+        Assert.assertEquals(config.eventMeshTcpMsgReqnumPerSecond, Integer.valueOf(4816));
+        Assert.assertEquals(config.eventMeshTcpClientMaxNum, 5816);
+        Assert.assertEquals(config.eventMeshTcpServerEnabled, Boolean.TRUE);
+        Assert.assertEquals(config.eventMeshTcpGlobalScheduler, 6816);
+        Assert.assertEquals(config.eventMeshTcpTaskHandleExecutorPoolSize, 7816);
+        Assert.assertEquals(config.eventMeshTcpMsgDownStreamExecutorPoolSize, 8816);
+        Assert.assertEquals(config.eventMeshTcpSessionExpiredInMills, 1816);
+        Assert.assertEquals(config.eventMeshTcpSessionUpstreamBufferSize, 11816);
+        Assert.assertEquals(config.eventMeshTcpMsgAsyncRetryTimes, 12816);
+        Assert.assertEquals(config.eventMeshTcpMsgSyncRetryTimes, 13816);
+        Assert.assertEquals(config.eventMeshTcpMsgRetrySyncDelayInMills, 14816);
+        Assert.assertEquals(config.eventMeshTcpMsgRetryAsyncDelayInMills, 15816);
+        Assert.assertEquals(config.eventMeshTcpMsgRetryQueueSize, 16816);
+        Assert.assertEquals(config.eventMeshTcpRebalanceIntervalInMills, Integer.valueOf(17816));
+        Assert.assertEquals(config.eventMeshServerAdminPort, 18816);
+        Assert.assertEquals(config.eventMeshTcpSendBackEnabled, Boolean.TRUE);
+        Assert.assertEquals(config.eventMeshTcpSendBackMaxTimes, 3);//默认
+        Assert.assertEquals(config.eventMeshTcpPushFailIsolateTimeInMills, 21816);
+        Assert.assertEquals(config.gracefulShutdownSleepIntervalInMills, 22816);
+        Assert.assertEquals(config.sleepIntervalInRebalanceRedirectMills, 23816);
+        Assert.assertEquals(config.eventMeshEventSize, 22816);
+        Assert.assertEquals(config.eventMeshEventBatchSize, 23816);
     }
 }
